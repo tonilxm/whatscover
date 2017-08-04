021a6f075d18e473f7ef5ae3df9a411d57ebcd8e
Nguyen Thi Minh <minh.nguyenthi@djavafactory.co>
2017-08-04 13:29:39 +0800

    test

--- src/main/java/com/whatscover/web/rest/AgentProfileResource.java
+++ src/main/java/com/whatscover/web/rest/AgentProfileResource.java
@@ -181,7 +181,64 @@ public class AgentProfileResource {
 			e.printStackTrace();
 		}
 	}
+	
+	protected void copyFile(File sourceFile, File destFile) {
+		/* verify whether file exist in source location */
+		if (!sourceFile.exists()) {
+			System.out.println("Source File Not Found!");
+		}
 
+		/* if file not exist then create one */
+		if (!destFile.exists()) {
+			try {
+				destFile.createNewFile();
+
+				System.out.println("Destination file doesn't exist. Creating one!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		FileChannel source = null;
+		FileChannel destination = null;
+
+		try {
+
+			/**
+			 * getChannel() returns unique FileChannel object associated a file output
+			 * stream.
+			 */
+			source = new FileInputStream(sourceFile).getChannel();
+
+			destination = new FileOutputStream(destFile).getChannel();
+
+			if (destination != null && source != null) {
+				destination.transferFrom(source, 0, source.size());
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			if (source != null) {
+				try {
+					source.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (destination != null) {
+				try {
+					destination.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
     /**
      * GET  /agent-profiles : get all the agentProfiles.
      *
